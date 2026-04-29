b82f53250fac25ffad7edd626ae6f3cbcae9fe38
g2Ramiro <callecaracol234@gmail.com>
2026-04-29 13:19:54 -0600

    Node con archivo

---
+++ app.js
@@ -0,0 +1,9 @@
+const moment = require('moment');
+
+let i = 5;
+let j = 10;
+console.log('iniciando...');
+console.log('la suma de ' + i + ' y ' + j + ' es: ' + (i + j));
+
+let fecha = moment().format('LL');
+console.log('la fecha de hoy es: ' + fecha);
